50e4845a4cf24596795258ef243b3a0f530d7ceb
root <xieshijie399@gmail.com>
2025-01-13 19:46:54 +0000

    remove unnecessary files

--- build.sh
+++ build.sh
@@ -2,39 +2,37 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
 mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j1
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j3
 
-cd ../../g2o
+cd ../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j1
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j3
 
-cd ../../Sophus
+cd ../Sophus
 
 echo "Configuring and building Thirdparty/Sophus ..."
 
 mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j1
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j3
 
-cd ../../../
+cd ../../
 
 echo "Uncompress vocabulary ..."
 
 cd Vocabulary
-tar -xf ORBvoc.txt.tar.gz
+if [ -f "*.txt" ]; then
+	tar -xf ORBvoc.txt.tar.gz
+fi
 cd ..
 
 echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j1
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j3
